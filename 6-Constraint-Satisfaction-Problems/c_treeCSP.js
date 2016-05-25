53b82dc8567f63547cd21dac1256e7de6716e047
Souradeep-Nanda <souradeep_nanda@hotmail.com>
2016-05-25 11:34:28 +0530

    Update 2016-05-25T06:04:23.456Z

--- 6-Constraint-Satisfaction-Problems/c_treeCSP.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-	$.ajax({
-		url : "treeCSP.js",
-		dataType: "text",
-		success : function (data) {
-			$("#treeCSPCode").html(data);
-		}
-	});
-});
--- 6-Constraint-Satisfaction-Problems/treeCSP.js
+++
@@ -1,1 +0,0 @@
-// Code for tree CSP
